ae04e4fec449eef01e08fdfcc8a7bab7572bdf8a
Robson <robhenrod@gmail.com>
2020-08-31 17:05:11 -0300

    Conversor de texto simples
    - App para converter texto em formato de frase, palavras capitalizada, maiúsculas e minusculas, letras alternadas e inversão das letras.

---
+++ src/components/ButtonDefault/index.tsx
@@ -0,0 +1,17 @@
+import React, { ButtonHTMLAttributes } from 'react';
+
+import './styles.css';
+
+interface ButtonDefaultProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+    text: string;
+}
+
+const ButtonDefault: React.FC<ButtonDefaultProps> = ({ text, ...rest }) => {
+    return (
+        <button className="btn-default" {...rest}>
+            {text}
+        </button>
+    );
+}
+
+export default ButtonDefault;
