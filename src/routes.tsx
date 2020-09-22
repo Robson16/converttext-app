c73ecb0bc28af75e4ea9e982d35aa4d40b3fcc50
Robson <robhenrod@gmail.com>
2020-09-22 16:40:41 -0300

    Github Pages Fixed
    GitHub Pages doesn’t support routers that use the HTML5 pushState history API under the hood (for example, React Router using browserHistory).

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,15 +1,15 @@
 import React from "react";
-import { BrowserRouter, Route } from "react-router-dom";
+import { HashRouter, Route } from "react-router-dom";
 
 import ConvertText from "./pages/ConvertText";
 import BinaryTranslater from "./pages/BinaryTranslater";
 
 function Routes() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Route path="/" exact component={ConvertText} />
       <Route path="/binary-translator" component={BinaryTranslater} />
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
