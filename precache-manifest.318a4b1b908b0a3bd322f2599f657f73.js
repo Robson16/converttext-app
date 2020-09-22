0a807a5206aabda7c6203c5a2bc0272221b0ebc9
Robson <robhenrod@gmail.com>
2020-09-22 15:27:27 -0300

    Updates

---
+++ precache-manifest.318a4b1b908b0a3bd322f2599f657f73.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28abd9f7455782681b0a247ca17cffcd",
+    "url": "/converttext-app/index.html"
+  },
+  {
+    "revision": "ece3f43139d48ff31667",
+    "url": "/converttext-app/static/css/main.765ed4a0.chunk.css"
+  },
+  {
+    "revision": "70b3ae98b4406e829b67",
+    "url": "/converttext-app/static/js/2.60831ab9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/converttext-app/static/js/2.60831ab9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ece3f43139d48ff31667",
+    "url": "/converttext-app/static/js/main.493362f3.chunk.js"
+  },
+  {
+    "revision": "7b39a783d4635709a8dd",
+    "url": "/converttext-app/static/js/runtime-main.f7e5f890.js"
+  }
+]);
